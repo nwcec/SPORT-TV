5b50234b3af8c35b99bc0b894d4629976bbab9bb
nwcec <91267870+nwcec@users.noreply.github.com>
2022-04-16 17:21:38 +0300

    Add files via upload

---
+++ scriptlive.js
@@ -0,0 +1,24 @@
+var myFP = fluidPlayer(
+        'video-id',	{
+	"layoutControls": {
+		"controlBar": {
+			"autoHideTimeout": 3,
+			"animated": true,
+			"autoHide": true
+		},
+		"htmlOnPauseBlock": {
+			"html": null ,
+			"height": "" ,
+			"width": null
+		},
+		"autoPlay": true,
+		"mute": true,
+		"allowTheatre": false,
+		"playPauseAnimation": true,
+		"playbackRateEnabled": false,
+		"allowDownload": false,
+		"playButtonShowing": true,
+		"fillToContainer": false,
+		"posterImage": "https://www.zyadda.com/wp-content/uploads/2019/12/%D8%AA%D8%B1%D8%AF%D8%AF-%D9%82%D9%86%D9%88%D8%A7%D8%AA-bein-sport-%D8%A7%D9%84%D9%85%D8%B4%D9%81%D8%B1%D8%A9-%D9%88%D8%A7%D9%84%D8%BA%D9%8A%D8%B1-%D9%85%D8%B4%D9%81%D8%B1%D8%A9.jpg"
+	},
+})
